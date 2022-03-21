ab5e6c1e81206023db9855ff7a8d9c8ff2f04010
Mohammad ْAl Hallaq <diab.4869@gmail.com>
2022-03-21 22:28:27 +0200

    init

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Core from '../modules/core/store'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    Core
+  }
+})
